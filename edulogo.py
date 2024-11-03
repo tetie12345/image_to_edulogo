9f601bb7f939338c8bb9bd6f42e07c852898deb4
tetie12345 <floristabak@gmail.com>
2024-11-03 19:43:59 +0100

    added output file functionality

--- edulogo.py
+++ edulogo.py
@@ -3,13 +3,16 @@ from math import *
 import sys
 import os
 
-flag = ""
+flag = ("", "")
 argc = len(sys.argv)
+i = 0
+outputFile = "out.txt"
 
-for i in range(len(sys.argv)):
+while i < len(sys.argv):
     if "-" in sys.argv[i]:
-        flag = sys.argv[i]
+        flag = (sys.argv[i], i)
         sys.argv.pop(i)
+    i+=1
 
 if argc < 2:
     print("missing arguments")
@@ -23,8 +26,15 @@ if flag == "-h":
     print(" -o <file>     specify the output file")
     quit()
 
-elif flag == "-o":
-    pass
+print(flag)
+if flag[0] == "-o":
+    print(argc)
+    if argc > flag[1] + 1:
+        outputFile = sys.argv[flag[1]]
+        sys.argv.pop(flag[1])
+    else:
+        print("missing arguments")
+        quit()
 
 img = Image.open(sys.argv[1])
 
@@ -33,25 +43,27 @@ img = img.convert("RGB")
 
 pixels = img.load()
 
+if os.path.isfile(outputFile): os.remove(outputFile)
 
+with open(outputFile, "x") as file:
 
-print(f'cs\npu\nsetposition [-{ceil(width/2)} {ceil(height/2)}]\npd\nmake "w {width}\nmake "c 0')
+    file.write(f'cs\npu\nsetposition [-{ceil(width/2)} {ceil(height/2)}]\npd\nmake "w {width}\nmake "c 0\n')
 
-steps = 1
-lastcolor = (-1, -1, -1)
-colors = []
+    steps = 1
+    lastcolor = (-1, -1, -1)
+    colors = []
 
-for i in range(width):
-    for j in range(height):
-        if (pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]) == lastcolor:
-            print("n")
-            continue
+    for i in range(width):
+        for j in range(height):
+            if (pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]) == lastcolor:
+                file.write("n\n")
+                continue
 
-        if (pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]) in colors:
-            print(f"r {colors.index((pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]))}")
-        else:
-            colors.append((pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]))
-            print(f"s [{pixels[i,j][0]} {pixels[i,j][1]} {pixels[i,j][2]}]")
-        lastcolor = (pixels[i,j][0], pixels[i,j][1], pixels[i,j][2])
-    print(f"e :w")
+            if (pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]) in colors:
+                file.write(f"r {colors.index((pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]))}\n")
+            else:
+                colors.append((pixels[i,j][0], pixels[i,j][1], pixels[i,j][2]))
+                file.write(f"s [{pixels[i,j][0]} {pixels[i,j][1]} {pixels[i,j][2]}]\n")
+            lastcolor = (pixels[i,j][0], pixels[i,j][1], pixels[i,j][2])
+        file.write(f"e :w\n")
 
